26ade7e04b4adb1d46ea3edacb5f035003f3778f
jihad4040 <mohammadjihad4040@gmail.com>
2026-05-10 17:13:31 +0600

    Make Route For Ai

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,7 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
+import { GetCurrentUser } from 'src/common/decorator/get-current-user.decorator';
+import { ApiBearerAuth } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService) { }
+
+  @Get('onboarding-log-temperature-for-ai')
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
+  async getUserOnbordingAndLogtemperatureForAi(@GetCurrentUser('userId') userId: string) {
+    return this.userService.getUserOnbordingAndLogtemperatureForAi(userId);
+  }
+
 }
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,4 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+
+    constructor(private readonly prisma: PrismaService) { }
+
+
+    async getUserOnbordingAndLogtemperatureForAi(userId: string) {
+        const user = await this.prisma.user.findUnique({
+            where: {
+                userId: userId
+            },
+            include: {
+                onbording: true,
+                logTemperature: true
+            }
+        });
+
+        if (!user) {
+            throw new NotFoundException(`User not found`);
+        }
+
+        const { password, otp, refreshToken, ...rest } = user;
+
+        return rest;
+
+    }
+
+}
