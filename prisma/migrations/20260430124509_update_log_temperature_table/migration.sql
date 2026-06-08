5cbfc75dcd40303ba1a22790647119584f174b56
= <=>
2026-06-08 09:38:57 +0600

    Baseline database migrations

--- prisma/migrations/20260430124509_update_log_temperature_table/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Changed the type of `date` on the `LogTemperature` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
-
-*/
--- AlterTable
-ALTER TABLE "LogTemperature" DROP COLUMN "date",
-ADD COLUMN     "date" DECIMAL(4,1) NOT NULL;
--- prisma/migrations/20260513120823_add_soft_delete_to_group_category/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "group_categories" ADD COLUMN     "isDeleted" BOOLEAN NOT NULL DEFAULT false;
-
--- CreateIndex
-CREATE INDEX "group_categories_isDeleted_idx" ON "group_categories"("isDeleted");
